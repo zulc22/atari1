d1ecf15e57949e4d51bcd7bfd3569d81f3e88021
KScott <kscott.db@gmail.com>
2020-05-18 21:24:34 -0700

    login and signup forms, site is prettier

--- atari1.py
+++ atari1.py
@@ -18,11 +18,35 @@ messages = [
 def timestring():
     return time.strftime("%m/%d/%Y %I:%M:%S %p (%Z)")
 
+def generate_header(pagename):
+    return render_template('header.html',
+        message=random.choice(messages),
+        time=timestring(),
+        pagename=pagename
+    )
+
 @app.route('/')
 def index():
+    pagename = "⌂ index"
+
     return render_template('index.html',
-        message=random.choice(messages),
-        time=timestring()
+        headerhtml=generate_header(pagename)
+    )
+
+@app.route('/login')
+def loginform():
+    pagename = "※ login"
+
+    return render_template('loginform.html',
+        headerhtml=generate_header(pagename)
+    )
+
+@app.route('/signup')
+def signupform():
+    pagename = "✆ sign up"
+
+    return render_template('signupform.html',
+        headerhtml=generate_header(pagename)
     )
 
 @app.route('/api/restart')
